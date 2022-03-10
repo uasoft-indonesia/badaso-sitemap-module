2a71f0d7e23ed6111650f9120ed24561ba0dcdfc
Rizki Heryandi <rizkiheryandi@gmail.com>
2022-03-10 12:11:05 +0700

    test2

--- tests/Feature/BadasoSiteMapTest.php
+++ tests/Feature/BadasoSiteMapTest.php
@@ -142,6 +142,7 @@ class BadasoSiteMapTest extends TestCase
                     }
                     
                 }
+                
                 $sitemap = collect($sitemaparr);
                 $lastmod = collect($lastmodarr);
                
@@ -155,6 +156,7 @@ class BadasoSiteMapTest extends TestCase
                     });
 
                     $lastmod_data = $lastmod->first(function ($items) use ($last) {
+                        dd($items,$last);
                         $item = substr(str_replace('T', ' ', $items), 0, 19);
 
                         return $item == $last;
